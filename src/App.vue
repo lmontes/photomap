58c5f347bbd3a601205785593742089eb3d32217
lmontes <6731463+lmontes@users.noreply.github.com>
2023-07-29 12:40:40 +0200

    Solve error with dependencies

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,6 @@
   <div class="flex justify-start">
     <div class="w-screen h-screen mx-auto">
       <photomap></photomap>
-      <!--div class="relative">
-        <button class="
-          absolute
-          left-0
-          inset-y-0
-          flex
-          items-center
-          px-4
-          text-white
-          hover:text-gray-800
-          cursor-pointer
-          text-4xl
-          font-extrabold
-          bg-black
-          text-white
-        ">Hahah</button>
-      </div-->
     </div>
   </div>
 </template>
